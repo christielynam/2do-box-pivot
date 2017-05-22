c85ffceff3ce0d96e84f69a3a189b0a02164063c
Jack Mallahan <jackmallahan@gmail.com>
2017-05-22 17:24:58 -0600

    Applied prepend to save button

---
+++ script.js
@@ -0,0 +1,55 @@
+//*********************Variables***************************//
+var ideaTitle = $('.title-input').val();
+var ideaBody = $('.body-input').val();
+var myIdeas = []
+
+//*********************EVENT LISTENERS**********************//
+
+//create new card on button click
+$('.save-button').on('click', function(){
+  newIdeaCard();
+})
+
+// $('button').on('click', function() {
+//   console.log('array before adding: ', myIdeas)
+//   var newIdea = {title: $(('.title-input').val()), body: $('.body-input').val()}
+
+//**********************Functions**************************//
+
+//function for creating new idea card
+function newIdeaCard(){
+  var ideaTitle = $('.title-input').val();
+  var ideaBody = $('.body-input').val();
+  var ideaCard = $('.idea-container').prepend(`
+    <article class="idea-card">
+      <div class="card-top">
+        <h2>${ideaTitle}</h2>
+        <img src="assets/delete.svg" class="delete icon">
+        <img src="assets/delete-hover.svg" class="delete-hover">
+      </div>
+      <p class="idea-text">${ideaBody}</p>
+      <div class="card-bottom">
+        <img src="assets/upvote.svg" class="up-vote icon">
+        <img src="assets/upvote-hover.svg" class="up-vote-hover icon">
+        <img src="assets/downvote.svg" class="down-vote icon">
+        <img src="assets/downvote-hover.svg" class="down-vote-hover icon" class="down-vote-hover"
+        <p class="quality">quality: swill</p>
+      </div>
+      <hr>
+    </article>
+    `)
+  };
+
+//******************Local Storage***************************//
+
+
+
+// var Idea = function(title, body){
+//   this.title = title;
+//   this.body = body;
+// };
+//
+// var stringifiedIdeas = JSON.stringify(ideaTitle)
+// var parsedIdeas = JSON.parse()
+//
+// localStorage.setItem('ideas', stringifiedIdeas)
