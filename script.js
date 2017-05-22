a42549c4c89c64474abbae1291933dc381a7ba5f
Jack Mallahan <jackmallahan@gmail.com>
2017-05-22 17:56:02 -0600

    Completed storing to JSON, need to bring it back to Javascript

--- script.js
+++ script.js
@@ -2,17 +2,66 @@
 var ideaTitle = $('.title-input').val();
 var ideaBody = $('.body-input').val();
 var myIdeas = []
+// var ideaCard = $('.idea-container').prepend(`
+//   <article class="idea-card">
+//     <div class="card-top">
+//       <h2>${ideaTitle}</h2>
+//       <img src="assets/delete.svg" class="delete icon">
+//       <img src="assets/delete-hover.svg" class="delete-hover">
+//     </div>
+//     <p class="idea-text">${ideaBody}</p>
+//     <div class="card-bottom">
+//       <img src="assets/upvote.svg" class="up-vote icon">
+//       <img src="assets/upvote-hover.svg" class="up-vote-hover icon">
+//       <img src="assets/downvote.svg" class="down-vote icon">
+//       <img src="assets/downvote-hover.svg" class="down-vote-hover icon" class="down-vote-hover"
+//       <p class="quality">quality: swill</p>
+//     </div>
+//     <hr>
+//   </article>
+//   `)
 
 //*********************EVENT LISTENERS**********************//
 
 //create new card on button click
-$('.save-button').on('click', function(){
-  newIdeaCard();
-})
+// $('.save-button').on('click', function(){
+//   newIdeaCard();
+// })
 
-// $('button').on('click', function() {
-//   console.log('array before adding: ', myIdeas)
-//   var newIdea = {title: $(('.title-input').val()), body: $('.body-input').val()}
+$('.save-button').on('click', function() {
+  console.log('array before adding: ', myIdeas)
+  var ideaTitle = $('.title-input').val();
+  var ideaBody = $('.body-input').val();
+  var newIdea = {title: ideaTitle, body: ideaBody}
+  var ideaCard = $('.idea-container').prepend(`
+    <article class="idea-card">
+      <div class="card-top">
+        <h2>${ideaTitle}</h2>
+        <img src="assets/delete.svg" class="delete icon">
+        <img src="assets/delete-hover.svg" class="delete-hover">
+      </div>
+      <p class="idea-text">${ideaBody}</p>
+      <div class="card-bottom">
+        <img src="assets/upvote.svg" class="up-vote icon">
+        <img src="assets/upvote-hover.svg" class="up-vote-hover icon">
+        <img src="assets/downvote.svg" class="down-vote icon">
+        <img src="assets/downvote-hover.svg" class="down-vote-hover icon" class="down-vote-hover"
+        <p class="quality">quality: swill</p>
+      </div>
+      <hr>
+    </article>
+    `)
+  console.log(newIdea)
+//push into the array
+  myIdeas.push(newIdea);
+  console.log(myIdeas);
+//stringify that shit
+  var stringifiedIdea = JSON.stringify(myIdeas);
+//add that shit to localStorage
+  localStorage.setItem('idea', stringifiedIdea)
+
+});
+
 
 //**********************Functions**************************//
 
