2f17328b2ec4a1a22102de4f2ad6c11116b70927
Christie Lynam <christielynam@gmail.com>
2017-05-30 19:18:35 -0600

    building out remainder of phase 3

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ $('.save-btn').on('click', function() {
   var title = $('.title-input').val();
   var task = $('.task-input').val();
   var newToDo = new ToDo(id, title, task);
-  toDoArray.push(newToDo);
-  buildNewCard(newToDo);
+  toDoArray.unshift(newToDo);
+  $('.todo-container').prepend(buildNewCard(newToDo));
   addToLocal();
   reset();
 });
@@ -28,8 +28,13 @@ $('.todo-container').on('click', '.up-vote-btn', changeImportance);
 //Down-Vote Event
 $('.todo-container').on('click', '.down-vote-btn', changeImportance);
 
-$(document).on('click', '.show-completed-btn', showCompletedToDos);
-$(document).on('click', '.hide-completed-btn', hideCompletedToDos)
+$(document).on('click', '.show-completed-btn', showAllToDos);
+$(document).on('click', '.clear-filter-btn', clearFilters);
+
+$(document).on('click', '.show-more-btn', showMoreToDos);
+
+// Filter importanceLevel
+$('.filter-btn-container').on('click', '.importance-btn', filterImportance);
 
 $('.todo-container').on('click', '.complete-btn', toggleComplete);
 
@@ -45,53 +50,49 @@ $('.todo-container').on('blur input keydown', '.task-content', updateCardContent
 function updateCardContent(event){
   if(event.keyCode == 13 || event.type == 'focusout')
   {
-      //console.log('this', $(this));
-      //console.log('target', $(event.target));
       var cardToUpdate = $(event.target).closest('.todo-card')[0];
       var propertyToUpdate = $(event.target).data("role"); //this snags the role off the data-role in html
       console.log('role:', propertyToUpdate);
-      //console.log('target', event.target);
       toDoArray.forEach(function(todo){
         if(cardToUpdate.id == todo.id){
           todo[propertyToUpdate] = $(event.target).text();
         }
-        // if(cardToUpdate.id == todo.id && event.target.className == 'task-content'){
-        //   todo[propertyToUpdate] = $(event.target).text();
-        // }
       });
       addToLocal();
       $('.title-input').focus();
   }
 }
 
-function showCompletedToDos() {
+function showToDos(status, count) {
+  var displayCount = 0;
+  if (!count) {
+    count = toDoArray.length;
+  }
+  toDoArray.forEach(function(todo){
+    if (todo.complete === status && displayCount < count){
+      $('.todo-container').append(buildNewCard(todo));
+      displayCount++;
+    }
+  });
+}
+
+function showAllToDos() {
   //destroy
   $('.todo-container').empty();
-  // this resorts the array and pushes all completed todos to front
-  toDoArray.sort(function(x, y) {
-    if (x.complete === y.complete){
-      return 0; //
-    } else if (x.complete) {
-      return 1; //
-    } else {
-      return -1; //
-    }
-    // this is the same...in shorthand...notice the x? this is the 2nd if
-    //return (x === y)? 0 : x? -1 : 1;
-  });
-  toDoArray.forEach(function(todo) {
-    buildNewCard(todo);
-  });
+  showToDos(true);
+  showToDos(false);
 };
 
-function hideCompletedToDos() {
+function clearFilters() {
   // destroy
   $('.todo-container').empty();
-  toDoArray.forEach(function(todo){
-    if (!todo.complete){
-      buildNewCard(todo);
-    }
-  });
+  showToDos(false, 10);
+  reset();
+}
+
+function showMoreToDos() {
+  $('.todo-container').empty();
+  showToDos(false);
 }
 
 function toggleComplete() {
@@ -145,27 +146,20 @@ function buildNewCard(newToDo){
           <p id="importance">importance: <span class="importance-level">${newToDo.importance}</span></p>
         </div>
       </article>`;
-  $('.todo-container').prepend(todo);
-};
+  return todo;
+}
 
 //add object to localStorage function
 function addToLocal(){
   var stringifiedToDo = JSON.stringify(toDoArray);
-  //console.log(stringifiedToDo)
   localStorage.setItem('toDoArray', stringifiedToDo);
-};
+}
 
 // get object back from JSON function
   function retrieveLocalStorage() {
     toDoArray = JSON.parse(localStorage.getItem('toDoArray')) || [];
-    toDoArray.forEach(function(todo) {
-      // exempt cards that are marked complete
-      if (!todo.complete) {
-        buildNewCard(todo);
-      }
-  })
-
-};
+    showToDos(false, 10);
+}
 
 //upvote button function
 function changeImportance(){
@@ -174,16 +168,10 @@ function changeImportance(){
   var changer = $(event.target).data("changer");
   console.log('changer', changer);
 
-  // if(event.target.className == 'up-vote-btn'){
-  //   changer = 1;
-  // } else {
-  //   changer = -1;
-  // }
 
    toDoArray.forEach(function(todo, index){
     if (todo.id == card.id) {
       var currIndex = importanceArray.indexOf(todo.importance);
-      //console.log('currindex', currIndex);
 
       if(currIndex == 0 && changer == -1){
         currIndex++;
@@ -194,7 +182,6 @@ function changeImportance(){
 
       todo.importance = importanceArray[currIndex + changer];
       var domLabel = $(event.target).parents('.todo-card').find('.importance-level')[0]
-      // console.log('domlabel', domLabel);
       domLabel.innerText = todo.importance;
       flashText(domLabel);
     }
@@ -211,27 +198,13 @@ function flashText(elementToFlash){
   elementToFlash.classList.add('flash');
 }
 
-//downvote button function
-function downVote(){
-  var importanceLevel = $('.importance-level')
-  if (importanceLevel.text() === 'genius'){
-    importanceLevel.text('plausible')
-  }else if (importanceLevel.text() === 'plausible'){
-    importanceLevel.text('swill')
-  }
-  addToLocal();
-}
-
-// Filter importanceLevel
-$('.filter-btn-container').on('click', '.importance-btn', filterImportance);
-
 
 function filterImportance() {
   var importance = $(this).text();
   $('.todo-container').empty();
   toDoArray.forEach(function(todo, index) {
     if (importance === todo.importance && !todo.complete) {
-      buildNewCard(todo);
+      $('.todo-container').append(buildNewCard(todo));
     }
   })
 }
@@ -239,15 +212,23 @@ function filterImportance() {
 //Search Bar Function
 function search() {
   var inputText = $('.filter-input').val().toUpperCase();
+
+
   var filteredArray = toDoArray.filter(function(todo){
     if ((todo.title.toUpperCase().indexOf(inputText) !== -1) || (todo.task.toUpperCase().indexOf(inputText) !== -1)) {
       return true;
     }
   });
+
   $('.todo-container').empty();
-  filteredArray.forEach(function(todo) {
-    buildNewCard(todo);
-  });
+
+  if (inputText.length == 0) {
+    showToDos(false, 10);
+  } else {
+    filteredArray.forEach(function(todo) {
+      $('.todo-container').append(buildNewCard(todo));
+    });
+  }
 }
 
 //enable the save button function
@@ -264,46 +245,7 @@ var task = $('.task-input').val();
 function reset(){
   $('.title-input').val('');
   $('.task-input').val('');
+  $('.filter-input').val('');
   // $('.save-btn').prop('disabled', true);
   $('.title-input').focus();
 }
-
-//*********************************************************************
-//******************  The Graveyard of Failed Ideas *******************
-//*********************************************************************
-
-
-//failed down-vote click
-// $('.idea-container').on('click', '.down-vote', function() {
-//   counter --;
-//   if(counter <= 0) {
-//     $('.down-vote').prop("disabled", true);
-//   }
-//   else if (counter > 0) {
-//     $('.down-vote').prop("disabled", false)
-//   }
-//   console.log(counter)
-// });
-
-//failed attempt to save upvote
-// function saveUpvote(){
-//   var uniqueQuality = document.querySelector('.quality');
-//   var uniqueTitle = document.querySelector('h2')
-//   var uniqueBody = document.querySelector('.idea-tet')
-//   console.log($(uniqueQuality).text());
-
-  // ideaArray.forEach(function(idea, index){
-  //   if (idea.quality === "swill"){
-  //     $(uniqueQuality).text('plausible');
-  //   }if (idea.quality === 'plausible'){
-  //     $(uniqueQuality).text('genius');
-  //   }
-  // })
-// };
-
-// // failed idea to check Storage on page load
-// function checkStorage () {
-//   var stringifiedArr = localStorage.getItem(ideaArray);
-//   ideaArray = JSON.parse(stringifiedArr) || [];
-//   console.log('check storage function: ' + ideaArray);
-// }
